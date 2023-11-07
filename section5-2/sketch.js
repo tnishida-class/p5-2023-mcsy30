bd6ad5dd7dd50dc0e093ac418cd2e880cb166be8
mcsy30 <2110829h@stu.kobe-u.ac.jp>
2023-11-07 12:11:13 +0900

    5-2changed

--- section5-2/sketch.js
+++ section5-2/sketch.js
@@ -6,9 +6,7 @@ function setup(){
   fill(0);
   crossmark(10, 10, 90, 90);
   ngmark(150, 50, 80);
-  let i =[]
-  let n = i
-  regularPolygon(n, 150,50,40);
+  regularPolygon(8,250,50,40);
 }
 
 function crossmark(x1, y1, x2, y2){
@@ -30,8 +28,8 @@ function ngmark(cx, cy, r){
 function regularPolygon(n,cx, cy, r){
   beginShape();
 
-  for(var i = 2; i < 20; i++){
-    let theta = TWO_PI * i * 2 / n - HALF_PI;
+  for(var i = 0; i < n; i++){
+    let theta = TWO_PI * i*1 / n - HALF_PI;
     let x = cx + cos(theta) * r;
     let y = cy + sin(theta) * r;
     vertex(x,y);
